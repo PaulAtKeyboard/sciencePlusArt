d893662d63743eae1c26454653a8dfc5ed76decc
PaulAtKeyboard <13568779+PaulAtKeyboard@users.noreply.github.com>
2026-05-21 20:32:58 +1200

    Initial deployment
    Initial deployment using one HTML file and its associated JavaScripts.

---
+++ dist/scrambleEffect.js
@@ -0,0 +1,14 @@
+/* global Scrambler */
+/* eslint-disable import/prefer-default-export, no-console */
+
+Scrambler({
+  target: '[data-scrambler]',
+  random: [1000, 2800],
+  speed: 200
+});
+
+Scrambler({
+  target: '[data-scrambler-menu]',
+  random: [1000, 1500],
+  speed: 100
+});
